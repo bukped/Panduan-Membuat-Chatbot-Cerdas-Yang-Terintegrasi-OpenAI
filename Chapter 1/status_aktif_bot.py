f668265132b4366b82fa085d4c410baba1cd6e3c
safwan <cruelpennywise@gmail.com>
2023-02-12 15:55:24 +0700

    C1/menampilkan-status-ketika-aktif

--- Chapter 1/status_aktif_bot.py
+++ Chapter 1/status_aktif_bot.py
@@ -1,7 +1,7 @@
 # Kode ini menunjukkan bagaimana program menentukan event "on_ready", yang akan dipanggil ketika bot Discord sukses terhubung dengan server.
 # Event ini didefinisikan menggunakan decorator "@client.event", yang menunjukkan bahwa event ini akan dipicu oleh objek "client" dari kelas "commands.Bot".
 # Pada event "on_ready", metode "start()" pada objek "change_status" akan dipanggil. Ini akan memulai tugas yang didefinisikan dalam tugas tersebut, seperti mengubah status bot.
-# Teks "Bot is online! as Pepper PEW PEW" akan dicetak pada output, menunjukkan bahwa bot sukses terhubung dengan server dan siap digunakan.
+# Teks "Bot is online! as Pepper PEW PEW" akan dicetak pada output, menunjukkan bahwa bot sukses terhubung dengan server dan siap digunakan
 
 @client.event
 async def on_ready():
