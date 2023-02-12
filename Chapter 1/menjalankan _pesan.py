43b286e892a21e59ce1a9851887d4654f44fd1b0
safwan <cruelpennywise@gmail.com>
2023-02-12 15:48:34 +0700

    C1/intents-untuk-menjalankan-pesan-di-discord

--- Chapter 1/menjalankan _pesan.py
+++ Chapter 1/menjalankan _pesan.py
@@ -1,6 +1,6 @@
 # Kode ini menunjukkan bagaimana program menentukan "intents" atau niat dari aplikasi Discord.
 # Intents adalah tipe dari data yang dapat diterima dan dikirimkan oleh aplikasi Discord. Misalnya, aplikasi mungkin ingin menerima informasi tentang member yang sedang online, atau konten pesan yang diterima.
-# Ketika aplikasi terhubung dengan server Discord, informasi intents ini akan digunakan untuk menentukan jenis data yang akan diterima dan dikirimkan.
+# Ketika aplikasi terhubung dengan server Discord, informasi intents ini akan digunakan untuk menentukan jenis data yang akan diterima dan dikirimkan
 
 intents = discord.Intents.all()
 intents.presences = True
